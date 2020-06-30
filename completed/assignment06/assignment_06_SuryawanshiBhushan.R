7cabe9b6a5009e7b1852c3414fb78c903d5c7f9f
BhushanGitHub <bhushan.suryawanshi@gmail.com>
2020-06-30 18:58:28 -0500

    Added assignment for week 5.

--- completed/assignment06/assignment_06_SuryawanshiBhushan.R
+++ completed/assignment06/assignment_06_SuryawanshiBhushan.R
@@ -28,40 +28,65 @@ ggplot(data = heights_df, aes(y = earn, x = age)) +
   geom_line(color='red', data = age_predict_df, aes(y=earn, x=age), size = 1.2)
 
 mean_earn <- mean(heights_df$earn)
+
+
 ## Corrected Sum of Squares Total
 sst <- sum((mean_earn - heights_df$earn)^2)
+sst
+
 ## Corrected Sum of Squares for Model
 ssm <- sum((mean_earn - age_predict_df$earn)^2)
+ssm
+
 ## Residuals
 residuals <- heights_df$earn - age_predict_df$earn
+
 ## Sum of Squares for Error
 sse <- sum(residuals^2)
+sse
+
 ## R Squared R^2 = SSM\SST
 r_squared <- ssm/sst
+r_squared
 
 ## Number of observations
 n <- nrow(heights_df)
+n
+
 ## Number of regression parameters
 p <- 2
 ## Corrected Degrees of Freedom for Model (p-1)
 dfm <- p - 1
+dfm
+
 ## Degrees of Freedom for Error (n-p)
 dfe <- n - p
+dfe
+
 ## Corrected Degrees of Freedom Total:   DFT = n - 1
 dft <- n-1
+dft
 
 ## Mean of Squares for Model:   MSM = SSM / DFM
 msm <- ssm/dfm
+msm
+
 ## Mean of Squares for Error:   MSE = SSE / DFE
 mse <- sse/dfe
+mse
+
 ## Mean of Squares Total:   MST = SST / DFT
 mst <- sst / dft
+mst
+
 ## F Statistic F = MSM/MSE
 f_score <- msm / mse
+f_score
 
 ## Adjusted R Squared R2 = 1 - (1 - R2)(n - 1) / (n - p)
 adjusted_r_squared <- 1 - (((1 - r_squared)*(n - 1)) / (n - p))
+adjusted_r_squared
 
 ## Calculate the p-value from the F distribution
 p_value <- pf(f_score, dfm, dft, lower.tail=F)
-
+p_value
